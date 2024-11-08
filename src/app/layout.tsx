695938af5a6ecc2fb1145fd27931eeb4db7ad326
Fn-front <reservoir-station@outlook.jp>
2024-11-08 23:47:42 +0900

    clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Noto_Sans_JP } from 'next/font/google';
-import { Inter } from "next/font/google";
 
 import '@/styles/app.scss'
 
-const inter = Inter({ subsets: ["latin"] });
-
 const noto = Noto_Sans_JP({
-  weight: ["400", "700"],
-  style: "normal",
-  subsets: ["latin"],
+  weight: ['400', '700'],
+  style: 'normal',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "テンプレ",
-  description: "テンプレ",
+  title: 'テンプレ',
+  description: 'テンプレ',
 };
 
 export default function RootLayout({
@@ -23,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang='ja'>
       <body className={noto.className}>
         <div className='l_global_container'>
           <main className='l_main'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h2 className="c_h2">テンプレ</h2>
+      <h2 className='c_h2'>テンプレ</h2>
     </>
   );
 }
