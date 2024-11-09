4ebf993a739158662ebdc847bc1c87f06c398fc5
Fn-front <reservoir-station@outlook.jp>
2024-11-09 17:21:32 +0900

    add: features pagination

---
+++ src/features/pagination/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { usePagination } from '@/functions/hooks/usePagination'
+import type { DataItem } from '@/functions/types';
+
+export const Pagination = () => {
+  const {
+    data,
+    error,
+    isLoading,
+    // currentPage,
+    // setCurrentPage,
+  } = usePagination<DataItem>({
+    id: '1',
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Failed to load data</div>;
+  if (!data) return null;
+
+  return (
+    <>
+      <ul>
+        {data.data.map((item) => (
+          <li key={item.id}>
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Pagination
