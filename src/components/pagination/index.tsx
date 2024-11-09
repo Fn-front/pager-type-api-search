ac22467a2a8861106fe02606e846f8f4d8195ea6
Fn-front <reservoir-station@outlook.jp>
2024-11-09 20:16:50 +0900

    add: current

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NextButton from './parts/next'
 import PrevButton from './parts/prev'
 import ListItem from './parts/list'
@@ -8,26 +8,34 @@ import ListItem from './parts/list'
 interface CustomPaginationProps {
   currentPage: number
   totalPages: number
-  onPageChange: (page: number) => void
+  onPageChange: React.Dispatch<React.SetStateAction<number>>
 }
 
 export function CustomPagination({
   currentPage,
   totalPages,
-  // onPageChange
+  onPageChange
 }: CustomPaginationProps) {
 
   const [current, setCurrent] = useState<number>(currentPage ?? 1)
 
+  useEffect(() => {
+    onPageChange(current)
+  }, [current, onPageChange])
+
   return (
     <>
-      <div className='pagination'>
+      <div className='pagination_wrapper'>
         <PrevButton
           current={current}
           setCurrent={setCurrent}
           totalPages={totalPages}
         />
-        <ListItem />
+        <ListItem
+          current={current}
+          setCurrent={setCurrent}
+          totalPages={totalPages}
+        />
         <NextButton
           current={current}
           setCurrent={setCurrent}
