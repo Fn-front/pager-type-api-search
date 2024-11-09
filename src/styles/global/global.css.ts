56b5e45acafc9f45ea110393307c2bffc0ccf858
Fn-front <reservoir-station@outlook.jp>
2024-11-09 13:45:24 +0900

    fix: global.css.ts

---
+++ src/styles/global/global.css.ts
@@ -0,0 +1,47 @@
+import { createGlobalTheme, globalStyle } from '@vanilla-extract/css';
+
+// グローバル変数の定義
+export const vars = createGlobalTheme(':root', {
+  colors: {
+    white: '255, 255, 255',
+    black: '25, 26, 26',
+    blackDefault: '#000',
+    greenLight: '0, 255, 0',
+  },
+  sizing: {
+    basePx: '8',
+  },
+});
+
+globalStyle('*', {
+  boxSizing: 'border-box',
+  padding: 0,
+  margin: 0,
+});
+
+globalStyle('html', {
+  fontSize: '62.5%',
+});
+
+globalStyle('html, body', {
+  maxWidth: '100vw',
+  overflowX: 'hidden',
+});
+
+globalStyle('body', {
+  color: `rgba(${vars.colors.white}, 1)`,
+  background: `rgba(${vars.colors.black}, 1)`,
+  fontSize: '1.6rem',
+  margin: 0,
+  fontWeight: 400,
+  lineHeight: 1.5,
+});
+
+globalStyle('a', {
+  color: 'inherit',
+  textDecoration: 'none',
+});
+
+globalStyle('summary', {
+  cursor: 'pointer',
+});
