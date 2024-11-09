e43f0f435a9bebb9b87aaefe00f7df4846c372c3
Fn-front <reservoir-station@outlook.jp>
2024-11-09 14:53:13 +0900

    add: axios instance

---
+++ src/lib/axios/axiosInstance.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { getAuthToken } from './auth';
+
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getAuthToken();
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default axiosInstance;
